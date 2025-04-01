aec700ef423ad789bcdcf0b2861f3fc15f1787fc
mylad13 <mfarjadnasab@gmail.com>
2025-04-01 12:51:57 -0400

    some namespace updates

--- launch/multiple_robot_turtle_navigation.launch.py
+++ launch/multiple_robot_turtle_navigation.launch.py
@@ -8,6 +8,7 @@ from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, TextSubstitution
 from launch_ros.actions import Node
+from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description() -> None:
     # Get the launch and rviz directories
@@ -18,8 +19,6 @@ def generate_launch_description() -> None:
 
     rviz_config_dir = os.path.join(turtle_nav2_bringup_dir, "rviz2", "rviz_navigation_namespaced.rviz")
 
-    # Names and poses of the robots
-    robots = [{"name": "robot1"}, {"name": "robot2"}, {"name": "robot3"}]
 
     # Common settings
     use_sim_time = LaunchConfiguration("use_sim_time", default="True")
@@ -30,28 +29,33 @@ def generate_launch_description() -> None:
     use_rviz = LaunchConfiguration("use_rviz")
     log_settings = LaunchConfiguration("log_settings", default="true")
 
-    declare_robot1_params_file_cmd = DeclareLaunchArgument(
-        "robot1_params_file",
-        default_value=os.path.join(
-            turtle_nav2_bringup_dir, "params", "nav2_multirobot_params_1.yaml"
-        ),
-        description="Full path to the ROS2 parameters file to use for robot1 launched nodes",
+    declare_robot1_name_cmd = DeclareLaunchArgument(
+        "robot1_name",
+        default_value="explorer1",
+        description="Name for robot1"
     )
-
-    declare_robot2_params_file_cmd = DeclareLaunchArgument(
-        "robot2_params_file",
-        default_value=os.path.join(
-            turtle_nav2_bringup_dir, "params", "nav2_multirobot_params_2.yaml"
-        ),
-        description="Full path to the ROS2 parameters file to use for robot2 launched nodes",
+    declare_robot2_name_cmd = DeclareLaunchArgument(
+        "robot2_name",
+        default_value="explorer2",
+        description="Name for robot2"
     )
+    declare_robot3_name_cmd = DeclareLaunchArgument(
+        "robot3_name",
+        default_value="rescuer1",
+        description="Name for robot3"
+    )
+    # Names and poses of the robots
+    robots = [
+        {"name": LaunchConfiguration("robot1_name")},
+        {"name": LaunchConfiguration("robot2_name")},
+        {"name": LaunchConfiguration("robot3_name")},]
 
-    declare_robot3_params_file_cmd = DeclareLaunchArgument(
-        "robot3_params_file",
+    declare_robot_params_file_cmd = DeclareLaunchArgument(
+        "robot_params_file",
         default_value=os.path.join(
-            turtle_nav2_bringup_dir, "params", "nav2_multirobot_params_3.yaml"
+            turtle_nav2_bringup_dir, "params", "nav2_multirobot_params.yaml"
         ),
-        description="Full path to the ROS2 parameters file to use for robot3 launched nodes",
+        description="Full path to the ROS2 parameters file to use for robot launched nodes",
     )
 
     declare_bt_xml_cmd = DeclareLaunchArgument(
@@ -75,15 +79,26 @@ def generate_launch_description() -> None:
     # Define commands for launching the navigation instances
     nav_instances_cmds = []
     for robot in robots:
-        params_file = LaunchConfiguration(robot["name"] + "_params_file")
+        robot_name = robot["name"]
+        params_file = LaunchConfiguration("robot_params_file")
 
+        param_substitutions = {
+            "voxel_layer.scan.topic": [robot_name, "/scan"],
+            "global_costmap.obstacle_layer.scan.topic": [robot_name, "/scan"]
+        }
+
+        configured_params = RewrittenYaml(
+            source_file=params_file,
+            param_rewrites=param_substitutions,
+            convert_types=True)
+        
         group = GroupAction(
             [
                 IncludeLaunchDescription(
                     PythonLaunchDescriptionSource(os.path.join(nav2_bringup_launch_dir, "rviz_launch.py")),
                     condition=IfCondition(use_rviz),
                     launch_arguments={
-                        "namespace": TextSubstitution(text=robot["name"]),
+                        "namespace": robot_name,
                         "use_namespace": "True",
                         "rviz_config": rviz_config_file,
                     }.items(),
@@ -93,11 +108,11 @@ def generate_launch_description() -> None:
                         os.path.join(turtle_nav2_bringup_dir, "launch", "turtle_navigation_individual.launch.py")
                     ),
                     launch_arguments={
-                        "namespace": robot["name"],
+                        "namespace": robot_name,
                         "use_namespace": "True",
-                        "map": TextSubstitution(text=robot["name"] + "/map"),
+                        "map": [robot_name, "/map"],
                         "use_sim_time": use_sim_time,
-                        "params_file": params_file,
+                        "params_file": configured_params,
                         "default_bt_xml_filename": default_bt_xml_filename,
                         "autostart": autostart,
                         "use_rviz": "False",
@@ -106,18 +121,12 @@ def generate_launch_description() -> None:
                     }.items(),
                 ),
 
-                LogInfo(condition=IfCondition(log_settings), msg=["Launching ", robot["name"]]),
-                LogInfo(condition=IfCondition(log_settings), msg=[robot["name"], " map yaml: ", map_yaml_file]),
-                LogInfo(condition=IfCondition(log_settings), msg=[robot["name"], " params yaml: ", params_file]),
-                
-                LogInfo(
-                    condition=IfCondition(log_settings),
-                    msg=[robot["name"], " behavior tree xml: ", default_bt_xml_filename],
-                ),
-                LogInfo(
-                    condition=IfCondition(log_settings), msg=[robot["name"], " rviz config file: ", rviz_config_file]
-                ),
-                LogInfo(condition=IfCondition(log_settings), msg=[robot["name"], " autostart: ", autostart]),
+                LogInfo(condition=IfCondition(log_settings), msg=["Launching ", robot_name]),
+                LogInfo(condition=IfCondition(log_settings), msg=[robot_name, " map yaml: ", map_yaml_file]),
+                LogInfo(condition=IfCondition(log_settings), msg=[robot_name, " params yaml: ", configured_params]),
+                LogInfo(condition=IfCondition(log_settings), msg=[robot_name, " behavior tree xml: ", default_bt_xml_filename]),
+                LogInfo(condition=IfCondition(log_settings), msg=[robot_name, " rviz config file: ", rviz_config_file]),
+                LogInfo(condition=IfCondition(log_settings), msg=[robot_name, " autostart: ", autostart]),
             ]
         )
 
@@ -127,15 +136,14 @@ def generate_launch_description() -> None:
     ld = LaunchDescription()
 
     # Declare the launch options
-
-    ld.add_action(declare_robot1_params_file_cmd)
-    ld.add_action(declare_robot2_params_file_cmd)
-    ld.add_action(declare_robot3_params_file_cmd)
-
+    ld.add_action(declare_robot_params_file_cmd)
     ld.add_action(declare_bt_xml_cmd)
     ld.add_action(declare_use_rviz_cmd)
     ld.add_action(declare_autostart_cmd)
     ld.add_action(declare_rviz_config_file_cmd)
+    ld.add_action(declare_robot1_name_cmd)
+    ld.add_action(declare_robot2_name_cmd)
+    ld.add_action(declare_robot3_name_cmd)
 
     for simulation_instance_cmd in nav_instances_cmds:
         ld.add_action(simulation_instance_cmd)
